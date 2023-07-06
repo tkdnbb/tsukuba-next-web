398582159bfef83e99107ff26f51d914fc4f8e16
tkdnbb <1811639092@qq.com>
2023-07-06 15:28:07 +0900

    update(component): edited LineRouteMap

--- src/components/organisms/LineRouteMap.tsx
+++ src/components/organisms/LineRouteMap.tsx
@@ -1,15 +1,15 @@
-import stationsData from "@/data/stations.json"
-import StationLabel, { StationLabelProps } from "../molecules/StationLabel"
+import stationsData from "@/data/stations.json";
+import StationLabel, { StationLabelProps } from "../molecules/StationLabel";
 
 const LineRouteMap = () => {
-	const stations = stationsData.slice(0, 18) as StationLabelProps[]
-	return (
-		<div className="flex flex-row-reverse">
-		  {
-				stations.map(({id, serial, enName, name, furigana}) => <StationLabel key={id} {...{id, serial, enName, name, furigana}} />)
-			}
-		</div>
-	)
-}
+  const stations = stationsData as StationLabelProps[];
+  return (
+    <div className="flex flex-row-reverse max-w-[90vw] overflow-x-auto">
+      {stations.map(({ id, serial, enName, name, furigana }) => (
+        <StationLabel key={id} {...{ id, serial, enName, name, furigana }} />
+      ))}
+    </div>
+  );
+};
 
-export default LineRouteMap
+export default LineRouteMap;
